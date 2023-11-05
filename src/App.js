a58ef112e9618dabde473b83db5c8cc4cfb4a4a7
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-11-05 16:18:01 -0300

    Deleting an Item: More Child-to-Parent Communication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import { useState } from 'react';
 
-const initialItems = [
+/*const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
   { id: 3, description: 'Charger', quantity: 1, packed: false },
-];
+];*/
 
 export default function App() {
+  //recarrega a pagina com os items por padrão
+  //const [items, setItems] = useState(initialItems);
+
   const [items, setItems] = useState([]);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((item) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -39,6 +46,7 @@ function Form({ onAddItems }) {
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
 
+    //child to parent communication (inverse data flow)
     onAddItems(newItem);
 
     setDescription('');
@@ -73,25 +81,25 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
